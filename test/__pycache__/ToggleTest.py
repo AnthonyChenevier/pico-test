e86230a11ed5baa0347ec5ad924c1197382ed3a2
AnthonyChenevier <anthony.chenevier@gmail.com>
2025-10-28 11:14:01 +1100

    Testing framework

---
+++ test/__pycache__/ToggleTest.py
@@ -0,0 +1,15 @@
+from machine import Pin
+from TestingSuite import PicoTestBase
+
+class ToggleTest(PicoTestBase):
+    def test_toggle(self):
+        print(f"Testing toggle")
+        pin = Pin("LED", Pin.OUT)
+        pin.toggle()
+        assert pin.value() == 1, "toggle value does not match expected (1)"
+
+    def test_toggle2(self):
+        print(f"Testing toggle")
+        pin = Pin("LED", Pin.OUT)
+        pin.toggle()
+        assert pin.value() == 0, "toggle value does not match expected (0)"
